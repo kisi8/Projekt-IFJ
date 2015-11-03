ccb6219929833c3f975f0ed8a0e7d8cb510d3ca8
Karlos123 <kareljiranek@seznam.cz>
2015-11-03 22:39:47 +0100

    zacetek

--- scanner první verze.c
+++ scanner první verze.c
@@ -1,75 +1,82 @@
+ /**
+  * Par obecnych zasad pro vsechny co budou na lex. analyzatoru 
+  * pracovat. Prosim komentujte (primerene ale ne zas malo) at se v tom ostatni
+  * vyznaji. Dale jsem bych rekl ze by bylo mozna lepsi to psat
+  * v anglictine (kod) a komentare v cestine/slovenstine. Aby z 
+  * z toho nevzniknul HONZIK CODE ("while not NASEL.POMLUKUK") :D
+  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
+
+
+
+// Typy lexikalnich jednotek nacitanych lexikalnim analyzatorem
 typedef enum
-  {
-   AUTO_tk,          // oznaceni 0;
-   CIN_tk,
-   COUT_tk,
-   DOUBLE_tk,
-   ELSE_tk,
-   FOR_tk,
-   IF_tk,
-   INT_tk,
-   RETURN_tk,
-   WHILE_tk,
-   DO_tk,
-   BOOL_tk,
-   TRUE_tk,
-   FALSE_tk,
-   BREAK_tk,
-   CONTINUE_tk,
-   
-   STRING_tk,
-   
-   OR_tk,
-   AND_tk,
-   PLUS_tk,
-   INKREMENT_tk,
-   MINUS_tk,
-   DEKREMENT_tk,
-   NOT_tk,
-   NEROVNASE_tk,
-   PRIRAZENI_tk,
-   ROVNASE_tk,
-   MENSI_tk,
-   MENSIROVNO_tk,
-   VYSTUP_tk,
-   VETSI_tk,
-   VETIROVNO_tk,
-   VSTUP_tk,
-   
-   STREDNIK_tk,
-   KRAT_tk,
-   L_K_ZAVORKA_tk,
-   R_K_ZAVORKA_tk,
-   L_S_ZAVORKA_tk,
-   R_S_ZAVORKA_tk,
-   
-   EOF_tk,
-   ERROR_tk           // oznaceni 41;
-   
-  } tLexem;
+{
+  // Klicova slova (auto, cin, cout, if, else, for, bool, while, do, 
+  // double, int, string, true, false, break, continue, return, and, or )
+  AUTO_tk, CIN_tk, COUT_tk, IF_tk, ELSE_tk, FOR_tk, BOOL_tk, WHILE_tk, DO_tk, 
+  DOUBLE_tk, INT_tk, STRING_tk, TRUE_tk, FALSE_tk, BREAK_tk, CONTINUE_tk, RETURN_tk,  
+  AND_tk, OR_tk,
 
+  // Identifikator
+  ID_tk
+    
+  // Cislesna sekvence
+  NUM_tk,
+
+  // Relacni operatory (  !=  ==  <  >  >=  <=  )
+  NOTEQUAL_tk, EQUAL_tk, LESS_tk, GREATER_tk, GREATEREQUAL_tk, LESSEQUAL_tk
+
+  // Ostatni operatory (  =  +  -  *  /  ++  --  ;   )
+  ASSIGN_tk, ADD_tk, SUB_tk, MUL_tk,  DIV_tk INC_tk, DEC_tk, SEMICOLON_tk, 
+   
+  // Zavorky (  (  )  {  } ) .......  LeftRoundBracket, RightRoundBracket, LeftCurlyBracket, RightCurlyBracket
+  LR_BRACKET_tk, RR_BRACKET_tk, LC_BRACKET, RC_BRACKET,
+
+  // Konec souboru
+  EOF_tk,
+
+  // Chyba tokenu?
+  ERROR_tk          
+   
+  } tLexeme;
+
+
+// Unie na ulozeni potrebnych tagu k tokenu
 typedef union
-  {
-   char id[50];
-   int cislo;
-   float des_cislo;
-   char * ptr_retezec;
-  } tHodnota;
+{ 
+   int num; // integer
+   float dec_num; // desetine cislo
+   char *string;  // retezec
+} tNumber;
 
+
+// Token = lexem s potrebnymi atributy
 typedef struct
-  {
-   tLexem lexem;
-   tHodnota hodnota;
-  } tToken;
+{
+  tLexeme lexeme; // typ lexemu
+  tNumber token_num; // hodnota lexemu (cislo, posloupnost znaku, )
+  int line_num; // cislo radku kde byl dany lexem nalezen (bude se hodit pro debug programu)
+
+  //int str[50]; // nalezeny lexem v nezmene podobe (cislo, identifikator ...)
+} tToken;
+
+
 
 tToken posli_token();
 
 //--------------------------------------- MAIN ---------------------------
 
+
+
+
+/**
+ * Lexikalni Analyzator
+ */
 int main()
 {
 
