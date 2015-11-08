057712dfeceb0693bb631da85272f9973d8fa7cc
FaramosCZ <mschorm@centrum.cz>
2015-11-08 15:35:34 +0100

    Update scanner.c
    Nyní rozpoznává všechny operátory a poznámky

--- scanner.c
+++ scanner.c
@@ -12,16 +12,157 @@
 #include <ctype.h>
 #include "scanner.h"
 
-tToken get_token()
+tToken get_token(FILE *source)
 {
   // tato funkce bude vracet nacetny strukturu TOKEN (ukazatel na jeji zacatek)
+  tToken token;
+  int znak;
+  
+  do
+    {
+     znak = fgetc(source);
+     
+     if(znak==EOF){ token.lexeme = EOF_tk; break;}                     // pokud najdeme EOF, vrátíme token EOF_tk
+     if(znak=='('){ token.lexeme = LR_BRACKET_tk; break;}              // (
+     if(znak==')'){ token.lexeme = RR_BRACKET_tk; break;}              // )    
+     if(znak=='{'){ token.lexeme = LC_BRACKET_tk; break;}              // {
+     if(znak=='}'){ token.lexeme = RC_BRACKET_tk; break;}              // } 
+     if(znak=='*'){ token.lexeme = MUL_tk,; break;}                    // *
+     if(znak==';'){ token.lexeme = SEMICOLON_tk; break;}               // ;
+     
+     
+     
+     if(znak=='|')
+       {
+        znak = fgetc(source);
+        if(znak=='|')
+          { token.lexeme = OR_tk; break; }                             // dodefinovat operátor ||
+        else
+          { token.lexeme = ERROR_tk; break; }
+       }            
+     
+     
+     
+     if(znak=='&')
+       {
+        znak = fgetc(source);
+        if(znak=='&')
+          { token.lexeme = AND_tk; break; }                            // dodefinovat operátor &&
+        else
+          { token.lexeme = ERROR_tk; break; }
+       }            
+     
+     
+     
+     if(znak=='+')
+       {
+        znak = fgetc(source);
+        if(znak=='+')
+          { token.lexeme = INC_tk; break; }                            // ++
+        else
+          { ungetc(znak, source); token.lexeme = ADD_tk; break; }      // +
+       }         
+     
+     
+     
+     if(znak=='-')
+       {
+        znak = fgetc(source);
+        if(znak=='-')
+          { token.lexeme = DEC_tk; break; }                            // --
+        else
+          { ungetc(znak, source); token.lexeme = SUB_tk; break; }      // -
+       }  
+       
+       
+       
+     if(znak=='!')
+       {
+        znak = fgetc(source);
+        if(znak=='=')
+          { token.lexeme = NOTEQUAL_tk; break; }                       // !=
+        else
+          { ungetc(znak, source); token.lexeme = NOT_tk; break; }      // dodefinovat operátor !
+       }         
+       
+       
+       
+     if(znak=='=')
+       {
+        znak = fgetc(source);
+        if(znak=='=')
+          { token.lexeme = EQUAL_tk; break; }                          // ==
+        else
+          { ungetc(znak, source); token.lexeme = ASSIGN_tk; break; }   // =
+       }         
+       
+       
+       
+     if(znak=='<')
+       {
+        znak = fgetc(source);
+        if(znak=='<')
+          { token.lexeme = INPUT_tk; break; }                          // dodefinovat operátor cin <<
+        else if(znak=='=')
+          { token.lexeme = LESSEQUAL_tk; break; }                      // <=  
+        else
+          { ungetc(znak, source); token.lexeme = LESS_tk; break; }     // <
+       }             
+       
+       
+       
+     if(znak=='>')
+       {
+        znak = fgetc(source);
+        if(znak=='>')
+          { token.lexeme = OUTPUT_tk; break; }                         // dodefinovat operátor cin >>
+        else if(znak=='=')
+          { token.lexeme = GREATEREQUAL_tk; break; }                   // >=  
+        else
+          { ungetc(znak, source); token.lexeme = GREATER_tk; break; }  // >
+       }       
+       
+       
+       
+     if(znak=='/')
+       {
+        znak = fgetc(source);
+        if(znak=='/')
+          { do{ znak = fgetc(source);} while(znak != 10 || znak != 13); }         // jednoradkova poznamka // ... , ukonceno CR nebo LF
+        else if(znak=='*')
+          {  
+           while(1)
+             { 
+              znak = fgetc(source);
+              if(znak=='*')
+                {
+                 znak = fgetc(source);
+                 if(znak=='/'){break;}
+                 else {ungetc(znak, source);}
+                }
+             } 
+          }                                                                        // viceradkovy komentar /* ... */  
+        else
+          { ungetc(znak, source); token.lexeme = DIV_tk; break; }                  // operator /
+       }         
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+            
+      
+    } while(znak != EOF);  
 
   
-  tToken token;
 
   return token;
-
-
 }
 
 
@@ -41,7 +182,7 @@ tToken get_token()
        {
         znak = fgetc(zdrojak);
         
-        if(znak==EOF){ token.lexem = EOF_tk; }                   // pokud najdeme EOF, vrátíme token EOF_tk
+        if(znak==EOF){ token.lexeme = EOF_tk; }                   // pokud najdeme EOF, vrátíme token EOF_tk
         
         if(isalpha(znak) || znak=='_')                           // nedokoncena fce na nacitani identifikatoru
           {
@@ -60,4 +201,3 @@ tToken get_token()
   }
 
  */
-
